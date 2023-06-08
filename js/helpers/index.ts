793c53c03b2891011258b184ae0710fd79636060
Steven Balagtas <scbalagtas@gmail.com>
2023-06-09 00:31:33 +1000

    feat: :zap: completed challenge

--- js/helpers/index.ts
+++ js/helpers/index.ts
@@ -6,17 +6,37 @@
 import { VOWELS } from "../const";
 
 /**
+ * little helper to validate input for helpers below
+ * @param letter
+ */
+const validateLetter = (letter: string) => {
+  if (letter.length > 1) throw new Error("Multiple characters were passed in");
+
+  if (!/^[a-zA-Z]$/.test(letter))
+    throw new Error("Character is not an alphabetic character");
+};
+
+/**
  * helper to check if a letter is a vowel (more efficient than checking for consonants)
  * @param letter
  * @returns boolean
  */
 export const isVowel = (letter: string) => {
-  if (letter.length > 1) throw new Error("Multiple characters were passed in");
-
-  if (!/^[a-zA-Z]$/.test(letter))
-    throw new Error("Character is not an alphabetic character");
+  validateLetter(letter);
 
   if (VOWELS.includes(letter.toLowerCase())) return true;
+  return false;
+};
+
+/**
+ * helper to check if a letter is capital
+ * @param letter
+ * @returns boolean
+ */
+export const isCapital = (letter: string) => {
+  validateLetter(letter);
+
+  if (letter !== letter.toLowerCase()) return true;
   return false;
 };
 
@@ -26,6 +46,54 @@ export const isVowel = (letter: string) => {
  * @returns string of the pig latin translated phrase
  */
 export const translate = (phrase: string) => {
-  // TODO: translate...
-  return phrase;
+  // split phrase into array of words
+  const words = phrase.split(" ");
+
+  // map through words and translate each one
+  const translatedWords = words.map((word) => {
+    // get the individual characters
+    const characters = word.split("");
+
+    // don't need to know which specific characters to move but only how many to move
+    let numOfCharactersToMove = 0;
+
+    // iterate through the characters of the word and decide whether to shift characters
+    for (const character of characters) {
+      // takes into account grammar characters like commas and apostrophes at the end of words
+      // (rare case though as only a handful of english words do not have a vowel
+      // and even less or maybe none at all do not have a vowel before an apostrophe)
+      // should be an impossible case, but just in case
+      if (!/^[a-zA-Z]$/.test(character)) continue;
+
+      // if vowel, break
+      if (isVowel(character)) break;
+
+      numOfCharactersToMove++;
+    }
+
+    let translatedWord;
+    if (numOfCharactersToMove) {
+      // consonants case (add "ay")
+      const charactersToMove = word.substring(0, numOfCharactersToMove);
+      let remainingCharacters = word.substring(numOfCharactersToMove);
+
+      // bonus challenge check, if original word started with a capital, translated word must also start with a capital
+      if (isCapital(word.charAt(0))) {
+        remainingCharacters =
+          remainingCharacters.charAt(0).toUpperCase() +
+          remainingCharacters.substring(1);
+      }
+
+      // construct the pig latin translation
+      translatedWord =
+        remainingCharacters + charactersToMove.toLowerCase() + "ay";
+    } else {
+      // vowel case (add "way")
+      translatedWord = word + "way";
+    }
+
+    return translatedWord;
+  });
+
+  return translatedWords.join(" ");
 };
--- js/test/translate.test.ts
+++ js/test/translate.test.ts
@@ -5,7 +5,7 @@
 
 import { expect } from "chai";
 import { PHRASES } from "../const";
-import { isVowel, translate } from "../helpers";
+import { isCapital, isVowel, translate } from "../helpers";
 
 describe("Translate English to Pig Latin", function () {
   Object.keys(PHRASES).map((phrase) => {
@@ -16,12 +16,24 @@ describe("Translate English to Pig Latin", function () {
 });
 
 describe("Error cases", function () {
-  it(`should throw 'Multiple characters were passed in'`, () => {
+  it(`should throw 'Multiple characters were passed in' for isVowel`, () => {
     expect(() => isVowel("ab")).to.throw("Multiple characters were passed in");
   });
 
-  it(`should throw 'Character is not an alphabetic character'`, () => {
+  it(`should throw 'Character is not an alphabetic character' for isVowel`, () => {
     expect(() => isVowel("1")).to.throw(
+      "Character is not an alphabetic character"
+    );
+  });
+
+  it(`should throw 'Multiple characters were passed in' for isCapital`, () => {
+    expect(() => isCapital("ab")).to.throw(
+      "Multiple characters were passed in"
+    );
+  });
+
+  it(`should throw 'Character is not an alphabetic character' for isCapital`, () => {
+    expect(() => isCapital("1")).to.throw(
       "Character is not an alphabetic character"
     );
   });
